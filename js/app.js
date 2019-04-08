6152502d5f180c8e1b36b1aed1f97d092f81bf24
Scott Reyell <smreyell@gmail.com>
2019-04-07 21:39:44 -0700

    deleteBookmark and validateForm were added

--- js/app.js
+++ js/app.js
@@ -44,8 +44,30 @@ function saveBookmark(e){
   // Clear form
   document.getElementById('myForm').reset();
 
+  // Re-fectch bookmarks
+  fetchBookmarks();
+
   // Prevent form from submitting
   e.preventDefault();
+}
+
+
+function deleteBookmark(url) {
+  // Get bookmarks form locla storage
+  var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+
+  // Loop through bookmarks
+  for(var i = 0; i < bookmarks.length; i++){
+    if(bookmarks[i].url == url){
+      bookmarks.splice(i, 1);
+    }
+  }
+
+  // Reset localStorage
+  localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+
+  // Re-fectch bookmarks
+  fetchBookmarks();
 }
 
 // Fetch bookmarks
@@ -68,4 +90,22 @@ function fetchBookmarks(){
                                   '</h3>'+
                                   '</div>';
   }
+}
+
+
+function validateForm(siteName, siteUrl){
+  if(!siteName || !siteUrl){
+    alert('Please fill in the form');
+    return false;
+  }
+
+  var expression = /[-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?/gi;
+  var regex = new RegExp(expression);
+
+  if(!siteUrl.match(regex)){
+    alert('Please use a valid url');
+    return false;
+  }
+
+  return true;
 }
